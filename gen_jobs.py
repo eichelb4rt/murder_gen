5e22edc7a8aa6153060a97124551c42af2a54485
eichelb4rt <maurice.wenig@gmail.com>
2022-09-14 17:19:37 +0200

    outputs pdf now

--- gen_jobs.py
+++ gen_jobs.py
@@ -1,32 +1,43 @@
+import os
 import random
+import subprocess
 
-children_file = "participants.txt"
-jobs_file = "jobs.txt"
-delim = "================================"
+PARTICIPANTS = "participants.txt"
+PDF_DIR = "jobs_pdf"
+JOB_LIST = "job_list.tex"
+JOBS_SRC = "jobs.tex"
+
 
 def main():
-    global children_file, jobs_file
     # read all children
-    with open(children_file, "r") as f:
+    with open(PARTICIPANTS, "r") as f:
         lines = f.readlines()
     # remove all line breaks
     lines = [line.replace("\n", "") for line in lines]
     # shuffle all the children
-    random.shuffle(lines)#
-    # now every child in a line kills the child from the next line
+    random.shuffle(lines)
+
+    # now every participant in a line kills the participant from the next line
     jobs = ""
     for i in range(len(lines)):
         murderer = lines[i]
         target = lines[(i + 1) % len(lines)]
         job = format_job(murderer, target)
         jobs += f"{job}\n"
+
     # write the jobs
-    with open(jobs_file, "w") as f:
+    with open(os.path.join(PDF_DIR, JOB_LIST), "w") as f:
         f.write(jobs)
+    # generate the pdf
+    cmd = ["pdflatex", "-interaction", "nonstopmode", f"-output-directory={PDF_DIR}", os.path.join(PDF_DIR, JOBS_SRC)]
+    proc = subprocess.Popen(cmd)
+    proc.communicate()
+
 
 def format_job(murderer, target):
     global delim
-    return f"{delim}\nKiller:\t{murderer}\nZiel:\t{target}\n{delim}\n"
+    return f"\\job{{{murderer}}}{{{target}}}"
+
 
 if __name__ == "__main__":
     main()
