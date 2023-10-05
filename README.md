9832be8e403581895858d305b9a5f0a818d5d088
eichelb4rt <maurice.wenig@gmail.com>
2023-10-05 18:46:10 +0200

    can send mails now, wrote some documentation

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ You can find an example of generated jobs in [`example.pdf`](./example.pdf).
 
 ## Usage
 
-Put participants in `participants.txt` (like in [`participants-template.txt`](./participants-template.txt)), then `python gen_jobs.py`.
+Put participants in `participants.txt` (like in [`participants-template.txt`](./participants-template.txt)), then `python src/gen_jobs.py`.
 You can then find all the jobs in `jobs_pdf/jobs.pdf`.
 
 ## Printing
@@ -15,3 +15,90 @@ This way, the result is a bunch of little cards.
 You can fold the cards twice using the horizontal line as illustrated below, so that the distributers of the jobs don't know their content.
 
 ![How to fold the cards](how_to_fold.png)
+
+If you only want to print the cards and you do not want to sent the jobs via email, then you can ignore `mail_config-template`, `mails-template.csv`, and `all_jobs_list/`.
+
+## Automatic Job Mails
+
+You can also automatically send emails with `python src/send_mails.py`. But for this you have to do a couple of things:
+
+- create `mail_config.json`
+- create `mails.csv`
+- fill `all_jobs_list/` with the generated job pdfs
+
+### Mail Config
+
+The mail config `mail_config.json` should contain the following fields:
+
+- `smtp_host`: the smtp server you want to send the mails with
+- `smtp_version`: the encryption protocol you want to use with the smtp server (possible values: `ssl`, `tls`)
+- `smtp_port`: the port you want to use on the smtp server
+- `sender_mail`: the mail address the emails are sent from
+- `password`: the password for the mail account
+- `language`: the language of the sent message (possible values: `english`, `german`)
+
+#### Gmail Example
+
+This is not supported by gmail anymore, but still a useful example for you to understand how to fill the mail config.
+
+```json
+{
+    "smtp_host": "smtp.gmail.com",
+    "smtp_version": "SSL",
+    "smtp_port": 465,
+    "sender_mail": "person.omega@gmail.com",
+    "password": "super_secret_password",
+    "language": "english"
+}
+```
+
+#### Friedrich-Schiller-Universität Jena Example
+
+This is a working template for students of the Friedrich-Schiller-Universität Jena.
+
+```json
+{
+    "smtp_host": "smtp.uni-jena.de",
+    "smtp_version": "TLS",
+    "smtp_port": 587,
+    "sender_mail": "firstname.lastname@uni-jena.de",
+    "password": "<your password>",
+    "language": "german"
+}
+```
+
+### Mails CSV
+
+Put the participants with their respective emails into `mails.csv`.
+Note that the names used in `mails.csv` have to match up exactly with the names used in `participants.csv`.
+You will get reminded if this is not the case.
+
+### Job Lists
+
+Put the generated job pdfs (found in `jobs/jobs.pdf`) into `all_jobs_list/` and rename them.
+Their filename will be used as the name of the group each file represents.
+
+#### Example
+
+If this is the file structure:
+
+```text
+all_job_lists
+ ┣ .gitignore
+ ┣ Main_Group.pdf
+ ┣ Subgroup_A.pdf
+ ┗ Subgroup_B.pdf
+```
+
+where all of the participants can be found in `Main_Group.pdf`, half of them in `Subgroup_A.pdf`, and the other hald in `Subgroup_B.pdf`, then a valid email could look like this:
+
+```text
+Hello Person H,
+
+here are your jobs for the murder game:
+
+group 'Subgroup_A': Person F
+group 'Main_Group': Person D
+
+Sincerely, the organisation team of the murder game
+```
--- src/send_mails.py
+++ src/send_mails.py
@@ -1,26 +1,33 @@
 import os
+import sys
 import glob
+import json
 import pandas as pd
 from pathlib import Path
 from collections import defaultdict
+
 import smtplib
 from email.message import EmailMessage
 
 from jobs import JobList
 from extract_jobs import extract_jobs_from_pdf
 
-MAILS_FILE = "mails.csv"
+
 JOB_LISTS_DIR = "all_job_lists"
+MAILS_FILE = "mails.csv"
+MAIL_CONFIG_FILE = "mail_config.json"
 
 
 def extract_job_lists() -> list[JobList]:
     """Extract all the job lists from the pdfs in the job list directory."""
 
+    print("Extracting job lists...")
     job_lists: list[JobList] = []
     for job_file in glob.glob(f"{JOB_LISTS_DIR}/*.pdf"):
         name = Path(job_file).stem
         jobs = extract_jobs_from_pdf(job_file)
         job_lists.append(JobList(name, jobs))
+        print(f"extracted {name}")
     return job_lists
 
 
@@ -42,26 +49,74 @@ def is_concurrent(mails: pd.DataFrame, missions: dict[str, list[tuple[str, str]]
     return people_in_mails == people_in_missions
 
 
-def build_mail_content(killer: str, missions: list[tuple[str, str]]) -> str:
-    msg_content = f"Hallo {killer},\n\nhier nochmal die Aufträge, die du am Anfang des Mörderspiels bekommen hast:\n\n"
-    for job_list_name, target in missions:
-        msg_content += f"Kreis {job_list_name}: {target}\n"
-    msg_content += "\nLG die Organisation der Mörderspiels"
+def build_mail_content(killer: str, missions: list[tuple[str, str]], language="english") -> str:
+    if language == "english":
+        msg_content = f"Hello {killer},\n\nhere are your jobs for the murder game:\n\n"
+        for job_list_name, target in missions:
+            msg_content += f"group '{job_list_name}': {target}\n"
+        msg_content += "\nSincerely, the organisation team of the murder game"
+    elif language == "german":
+        msg_content = f"Hallo {killer},\n\nhier nochmal die Aufträge, die du am Anfang des Mörderspiels bekommen hast:\n\n"
+        for job_list_name, target in missions:
+            msg_content += f"Kreis '{job_list_name}': {target}\n"
+        msg_content += "\nLG die Organisation der Mörderspiels" 
+    else:
+        print(f"unknown language: {language} (supported: english, german)")
+        exit(1)
     return msg_content
 
 
-def build_all_mails(sender: str, mails: pd.DataFrame, killer_to_missions: dict[str, list[tuple[str, str]]]) -> list[EmailMessage]:
-    msgs: list[EmailMessage] = []
+def send_mails(mails: pd.DataFrame, killer_to_missions: dict[str, list[tuple[str, str]]]):
+    # read the mail config
+    with open(MAIL_CONFIG_FILE, 'r') as f:
+        mail_config = json.load(f)
+    # build messages
+    messages: list[EmailMessage] = []
     for _, row in mails.iterrows():
-        killer, killer_mail = row['name'], row['mail']
-        msg = EmailMessage()
-        msg['Subject'] = "Mörderspiel Aufträge"
-        msg['From'] = sender
-        msg['To'] = killer_mail
-        msg_content = build_mail_content(killer, killer_to_missions[killer])
-        msg.set_content(msg_content)
-        msgs.append(msg)
-    return msgs
+            killer, killer_mail = row['name'], row['mail']
+            msg = EmailMessage()
+            msg['Subject'] = "Murder Jobs"
+            msg['From'] = mail_config['sender_mail']
+            msg['To'] = killer_mail
+            msg_content = build_mail_content(killer, killer_to_missions[killer], language=mail_config['language'])
+            msg.set_content(msg_content)
+            messages.append(msg)
+    # show a sample message
+    if len(messages) == 0:
+        print("no messages to send, exiting.")
+        exit(1)
+    print("example mail:\n")
+    print(f"from: {msg['From']}\nto: {msg['To']}\nsubject: {msg['Subject']}\n================================\n{msg.get_content()}\n================================")
+    # confirm that the sample message is alright
+    sys.stdout.write(f"Is this ok? (y/n): ")
+    while True:
+        choice = input().lower()
+        if choice == 'y':
+            break
+        elif choice == 'n':
+            print("Aborting.")
+            exit(1)
+        else:
+            sys.stdout.write("Please respond with 'y' or 'n': ")
+    # create the connection to the ssl server
+    if mail_config['smtp_version'].lower() == 'ssl':
+        server = smtplib.SMTP_SSL(mail_config['smtp_host'], port=mail_config['smtp_port'])
+    elif mail_config['smtp_version'].lower() == 'tls':
+        server = smtplib.SMTP(mail_config['smtp_host'], port=mail_config['smtp_port'])
+        server.ehlo()
+        server.starttls()
+    else:
+        print(f"Unknown smtp version: {mail_config['smtp_version']}")
+        exit(1)
+    # log into smtp server
+    print(f"Logging into '{mail_config['smtp_host']}' as '{mail_config['sender_mail']}'...")
+    server.login(mail_config['sender_mail'], mail_config['password'])
+    # send the messages
+    print("Sending mails...")
+    for msg in messages:
+        server.send_message(msg)
+    server.quit()
+    print("All mails sent!")
 
 
 def main():
@@ -75,7 +130,7 @@ def main():
     killer_to_missions = extract_missions(job_lists)
     assert is_concurrent(mails, killer_to_missions), f"mails and missions are not concurrent:\nMails: {set(mails['name'])}\nMissions: {set(killer_to_missions.keys())}"
     # print(build_mail_content("Person B", killer_to_missions["Person B"]))
-    build_all_mails("SENDER", mails, killer_to_missions)
+    send_mails(mails, killer_to_missions)
 
 
 if __name__ == "__main__":
